35a449f60276e7ae8393ea60361318cad4f73bc5
Katelynn <ktlnn.nguyen@gmail.com>
2020-10-17 12:39:20 -0400

    deploying app

--- README.md
+++ README.md
@@ -12,7 +12,6 @@ SO THAT I have quick access to their information.
 ```
 
 ## Business Context
-
 An employee or manager would benefit greatly from being able to view non-sensitive data about other employees. It would be particularly helpful to be able to filter employees by name.
 
 ## How Employee Directory Works 
---
+++ docs/precache-manifest.03b2c274ee8b8955734285d74a0c534c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded91eb8579fb5e83f2556871ef038f6",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "6ac5d9007289b75ec7fb",
+    "url": "/employee-directory/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "f18c9f4e88d2a30ea67d",
+    "url": "/employee-directory/static/css/main.23dee5db.chunk.css"
+  },
+  {
+    "revision": "6ac5d9007289b75ec7fb",
+    "url": "/employee-directory/static/js/2.71f18916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.71f18916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18c9f4e88d2a30ea67d",
+    "url": "/employee-directory/static/js/main.6f32529a.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
